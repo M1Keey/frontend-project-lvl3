8b0d6c11dc1747d427a14cf5d8ea49bc485aad20
Artem Khabibullin <free2770@gmail.com>
2022-07-28 01:01:41 +0300

    removed extra filesa & fixes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
+import * as yup from 'yup';
+import axios from 'axios';
 import i18next from 'i18next';
 import { setLocale } from 'yup';
-import { handleAddLink } from './handlers.js';
 import view from './view.js';
 import resources from './locales/index.js';
+import parseRSS from './RSS_parcer.js';
 
 const app = () => {
   const defaultLanguage = 'ru';
@@ -37,9 +40,87 @@ const app = () => {
 
   const watchedState = view(state, i18nextInstance);
 
+  const validateLink = (url, feeds) => {
+    const urls = feeds.map(({ link }) => link);
+
+    const schema = yup.string().url().notOneOf(urls);
+
+    try {
+      schema.validateSync(url);
+      return null;
+    } catch (e) {
+      return e.message;
+    }
+  };
+
+  const allOrigin = (url) => {
+    const result = new URL('/get', 'https://allorigins.hexlet.app');
+    result.searchParams.set('url', url);
+    result.searchParams.set('disableCache', true);
+    return result.toString();
+  };
+
+  const addPostId = (data) => {
+    const postsWithId = data.posts.map((post) => ({
+      id: _.uniqueId(),
+      ...post,
+    }));
+    return { ...data, posts: postsWithId };
+  };
+
+  const loadRSS = (link) => axios.get(allOrigin(link))
+    .then((response) => parseRSS(link, response.data.contents))
+    .then((parcedData) => addPostId(parcedData));
+
+  const updateRSS = (link) => {
+    const links = [];
+    links.push(link);
+    const promises = links.map(loadRSS);
+    Promise.all(promises)
+      .then((results) => {
+        const loadedPosts = results.flatMap(({ posts }) => posts);
+        const allPosts = _.union(loadedPosts, watchedState.posts);
+        const newPosts = _.differenceBy(allPosts, watchedState.posts, 'link');
+
+        if (newPosts.length > 0) {
+          watchedState.posts = [...newPosts, ...watchedState.posts];
+        }
+      })
+      .finally(() => {
+        setTimeout(() => updateRSS(link), 5000);
+      });
+  };
+
   const form = document.querySelector('.rss-form');
   form.addEventListener('submit', (e) => {
-    handleAddLink(e, watchedState, i18nextInstance);
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const link = formData.get('url').trim();
+
+    const error = validateLink(link, watchedState.feeds);
+    watchedState.form.error = error;
+    if (!error) {
+      watchedState.form.processState = 'pending';
+
+      loadRSS(link)
+        .then((rss) => {
+          const { feed, posts } = rss;
+          watchedState.feeds.unshift(feed);
+          watchedState.posts = [...posts, ...watchedState.posts];
+          watchedState.form.processState = 'success';
+          updateRSS(link);
+        })
+        .catch((err) => {
+          if (err.isAxiosError) {
+            watchedState.form.error = i18nextInstance.t('errors.requestErr');
+          } else {
+            watchedState.form.error = i18nextInstance.t('errors.invalidRSS');
+          }
+          watchedState.form.processState = 'failed';
+        });
+    } else {
+      watchedState.form.processState = 'failed';
+    }
   });
 };
 
--- src/handlers.js
+++ src/handlers.js
@@ -4,43 +4,8 @@ import loadRSS from './RSS_loader.js';
 import updateRSS from './RSS_updater.js';
 
 export const handleAddLink = (e, state, i18nextInstance) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const link = formData.get('url').trim();
-
-  const error = validateLink(link, state.feeds);
-  state.form.error = error;
-  if (!error) {
-    state.form.processState = 'pending';
-
-    loadRSS(link)
-      .then((rss) => {
-        const { feed, posts } = rss;
-        state.feeds.unshift(feed);
-        state.posts = [...posts, ...state.posts];
-        state.form.processState = 'success';
-        updateRSS(link, state);
-      })
-      .catch((err) => {
-        if (err.isAxiosError) {
-          state.form.error = i18nextInstance.t('errors.requestErr');
-        } else {
-          state.form.error = i18nextInstance.t('errors.invalidRSS');
-        }
-        state.form.processState = 'failed';
-      });
-  } else {
-    state.form.processState = 'failed';
-  }
+  
 };
 
-export const handleViewPost = (post, state) => {
-  state.activePostId = post.id;
-  if (!state.viewedIds.includes(post.id)) {
-    state.viewedIds.push(post.id);
-  }
-};
 
-export const handleCloseModal = (state) => {
-  state.activePostId = null;
 };
